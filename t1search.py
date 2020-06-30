2de8c175799dd87722242462089183d9b59f5ac0
Linda <stayathomegeek@gmail.com>
2020-06-30 10:59:18 -0400

    Completed Verifications

--- t1search.py
+++ t1search.py
@@ -1,3 +1,10 @@
+# Questions for Mike:
+#1. Do we need to run the test in other browsers?
+
+
+
+
+
 # Documentation: https://selenium-python.readthedocs.io/api.html#module-selenium.webdriver.remote.webdriver 
 # For info on webdriverwait https://stackoverflow.com/questions/26566799/wait-until-page-is-loaded-with-selenium-webdriver-for-python
 # More on webdriverwait until: https://selenium-python.readthedocs.io/waits.html#explicit-waits
@@ -5,13 +12,13 @@ import time
 from selenium.webdriver.common.touch_actions import TouchActions 
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
-  
+from selenium.webdriver.common.by import By  
 
 def chrome_code(url):     
         
     options = webdriver.ChromeOptions()
     options.add_argument("--ignore-certificate-errors")
-    options.add_argument("headless")  # When running headless, there are additional errors, which don't seem to be a problem
+    #options.add_argument("headless")  # When running headless, there are additional errors in the console, which don't seem to be a problem
     driver = webdriver.Chrome("c:\\data\\chromedriver\\chromedriver.exe", options=options)
     driver.get(url) 
     driver.implicitly_wait(60) # not sure which delay to use
@@ -24,26 +31,29 @@ def firefox_code():
     # uses GeckoDriver
     pass
 
-
 def main():
-    url = "https://solosegment.com/?s=solosegment_monitoring_test"
+    url = "https://solosegment.com/"
     driver = chrome_code(url)
-    if driver.current_url == url:
-        print("Page URL is correct")
-    driver.found_title =  driver.find_element_by_id('ss-search-title')   
-    driver.found_search = driver.find_element_by_class_name('screen-reader-text')
-    print("Search is", found_search)
-    print("Type is", type(driver.found_title))
-    print("Type of .text is", type(driver.found_title.text))   
-    print("Found Title is", driver.found_title.text)
-    print("Driver.current_url is", driver.current_url)
-    print("The URL I put in was", url)    
-    
+    elem=driver.find_element(By.XPATH, '//*[@id="search-6"]/form/label/input') 
+    elem.send_keys("solosegment_monitoring_test")  
+    elem.send_keys(Keys.ENTER)
+    #elem.send_keys(Keys.
+    time.sleep(60)
+
+    search_url = driver.current_url
+    if search_url == "https://solosegment.com/?s=solosegment_monitoring_test":
+        print("Found Search Results Page:", search_url)
+
 
 if __name__=="__main__":
     main()
 
  
+
+ 
+
+
+
 '''
 driver.current_url
 driver.title
@@ -55,4 +65,12 @@ driver.page_source
 driver.window_handles
 driver.current_window_handle
 driver.desired_capabilities
+ 
+
+//*[@id="search-6"]/form/label/input
+//*[@id="search-6"]/form/label/input
+
+// FULL XPATH
+/html/body/div[1]/header/div[1]/div/div/div/div[2]/div/form/label/input
 '''
+
