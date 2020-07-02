a0ca385d262b6d85c46ac932c06820de225a8144
Linda <stayathomegeek@gmail.com>
2020-07-02 07:22:21 -0400

    Modularize the code by creating functions

--- t1search.py
+++ t1search.py
@@ -8,86 +8,70 @@ TODO:
     To start building test reports on top of Selenium+Python, I would leverage the python unittest module.
     You will get a basic sample in Selenium documentation here.
     Then HTMLTestRunner module combined with unittest provides basic but robust HTML reports.
+5. Page objects and unittest https://selenium-python.readthedocs.io/page-objects.html    
 
-"""
- 
- 
 ## may or may not need these ### 
 from   selenium.webdriver.support.ui import Select
 from   selenium.common.exceptions import NoSuchElementException
 from   selenium.common.exceptions import NoAlertPresentException 
 import logging
 import json
+from selenium.webdriver.support import expected_conditions as EC
 ################
-
-
-#### these are in use #####
+""" 
+ 
 import time
 from selenium.webdriver.common.touch_actions import TouchActions 
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By  
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
-
-
 
 def chrome_setup(url): 
-    """ Chromedriver does not currently work with sendkeys in headless mode. See
-    https://bugs.chromium.org/p/chromedriver/issues/detail?id=2521
-    Firefox reportedly does work for this.
-    """
-      
+    """Chromedriver does not currently work with sendkeys in headless mode. See
+    https://bugs.chromium.org/p/chromedriver/issues/detail?id=2521 
+    """      
     options = webdriver.ChromeOptions()
     options.add_argument("--ignore-certificate-errors")   
     driver = webdriver.Chrome("c:\\data\\chromedriver\\chromedriver.exe", options=options)
     driver.get(url)          
-    return driver # ignore the handshake errors     
+    return driver # ignore the handshake errors   
 
-
-def firefox_setup():
-    # uses GeckoDriver
-    pass
-
-def main():
-    url = "https://solosegment.com/"      
-    driver = chrome_setup(url)   
-
-
-    ### Turn this into send the search arg function     
+def simulate_search(driver):   
     elem=driver.find_element(By.XPATH, '//*[@id="search-6"]/form/label/input') 
     elem.send_keys("solosegment_monitoring_test")  
-    elem.send_keys(Keys.ENTER) 
+    elem.send_keys(Keys.ENTER)  
 
+def verify_results(driver):    
+    # 1. Check on correct url which is https://solosegment.com/?s=solosegment_monitoring_test       
+    if driver.current_url == "https://solosegment.com/?s=solosegment_monitoring_test":
+        print("Found Search Results Page:", driver.current_url)  
 
-    # Now we should be in the search results page!     
-    # Check to see if we are on the correct url which is https://solosegment.com/?s=solosegment_monitoring_test   
-    # Turn this into verify url function  
-    search_url = driver.current_url
-    if search_url == "https://solosegment.com/?s=solosegment_monitoring_test":
-        print("Found Search Results Page:", search_url)    
- 
-
-    # Now check on the page-title which should be "Search Results for: <span>solosegment_monitoring_test</span>"     
-    #Looking for <h1 class="page-title">Search Results for: <span>solosegment_monitoring_test</span></h1>  
-    # Turn this into Find the Title function 
+    # 2. Check on the title 
     driver.implicitly_wait(10)
     element = driver.find_element(By.XPATH, '//*[@id="ss-search-title"]/h2')     
     if element.text == "Search Results":
-        print(f"Found Search Title: '{element.text}'")    
-    driver.set_page_load_timeout(20) # This helps prevent Error reading broker pipe 
+        print(f"Found Search Title: '{element.text}'")       
 
-
-
-    # Turn this into tearDown function
+def tear_down(driver): 
     time.sleep(20) # this just keeps the head up for 20 seconds
-    driver.quit() # do not use driver.close()
+    driver.set_page_load_timeout(20) # This helps prevent Error reading broker pipe 
+    driver.quit() # do not use driver.close()    
 
+def send_results():
+    pass        
 
-    ### Turn this into send results to the log file
-    pass
+def firefox_setup():
+    """Consider using firefox for sendkeys with headless. (Firefox driver is called GeckoDriver)"""  
+
+def main():
+    url = "https://solosegment.com/"      
+    driver = chrome_setup(url)    
+    simulate_search(driver)     
+    verify_results(driver)     
+    tear_down(driver)
+    send_results()
+     
 
 if __name__=="__main__":
     main()
