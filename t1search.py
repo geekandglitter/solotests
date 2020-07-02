0534fa818cce2bdd19768ce84aad9dd34deb75f9
Linda <stayathomegeek@gmail.com>
2020-07-02 13:27:11 -0400

    Added code to update an external log file

--- t1search.py
+++ t1search.py
@@ -3,20 +3,19 @@ DISCUSS WITH MIKE:
 1. This really should be written entirely in OOP, possibly with unittest as a testing framework 
 2. From Stackoverflow: HTMLTestRunner module combined with unittest provides basic but robust HTML reports.
 3. I haven't scratched the surface in how to log the results, but whatever I do will be too manual
+4. Is one log for repeated running of this one test? Do the results accumulate? Different log for the next Trello card?
+5. Best practices for updating the log file -- whree in my code do I do that? 
 
-UPDATE FROM MIKE:
-
-- The developers do prefer headless, but not if it means you can't test the ENTER key, so maybe keep it Chrome and headed for now.
-- You can initiate a search by clicking the search icon in the search input box as well as by pressing enter, so that should be maybe 
-created as an additional test.
-- Running from the command line is OK, at least for now.
-- Creating a log as we discussed is fine, too.
   
 
 TODO:    
 1. Logging format: search 1 pass/fail the fail message
 2. Continue with Colt's Modern Python 3 Bootcamp 
 3. Look into the free trial of Robot Framework or GhostInspector or Testcraft
+4. Add firefox code
+5. Fix the open file code to be OPEN WITH
+6. Mike said: You can initiate a search by clicking the search icon in the search input box as well as by pressing enter, 
+    so that should be maybe created as an additional test
 
 
 ## may or may not need these ### 
@@ -52,17 +51,20 @@ def simulate_search(driver, keyword):
     try:
         elem=driver.find_element(By.XPATH, '//*[@id="search-6"]/form/label/input') # We are looking inside the home page
     except (NoSuchElementException):
-        print("Something went wrong. Search box not found. ")     
+        print("Something went wrong. Search box not found. "  )   
         driver.quit()
         sys.exit(1)
     elem.send_keys(keyword)  
     elem.send_keys(Keys.ENTER)  
     return
 
-def verify_results(driver, keyword):    
+def verify_results(driver, keyword):
+       
     # 1. Check on correct url which is https://solosegment.com/?s=solosegment_monitoring_test       
     if driver.current_url == f"https://solosegment.com/?s={keyword}":
-        print("Found Search Results Page:", driver.current_url)  
+        msg_url =f"Found Search Results Page: {driver.current_url}"
+    else: 
+        msg_url = f"Did not find Search Results Page: https://solosegment.com/?s={keyword}"    
 
     # 2. Check on the title 
     driver.implicitly_wait(10)
@@ -74,8 +76,11 @@ def verify_results(driver, keyword):
         driver.quit()
         sys.exit(1)
     if elem.text == "Search Results":
-        print(f"Found Search Title: '{elem.text}'")       
-    return
+        msg_elem = f"Found Search Title: '{elem.text}'"     
+        
+    else: 
+        msg_elem = f"Did not find search title '{elem.text}'' "      
+    return msg_url, msg_elem
 
 def tear_down(driver): 
     time.sleep(20) # this just keeps the head up for 20 seconds
@@ -83,8 +88,10 @@ def tear_down(driver):
     driver.quit() # do not use driver.close()    
     return
 
-def send_results():
-    pass        
+def send_results(msg_url, msg_elem):
+    f= open("t1log.txt","w+")
+    f.write(f" {msg_url} \n {msg_elem}" )         
+    f.close()
 
 def firefox_setup():
     """Consider using firefox for sendkeys with headless. (Firefox driver is called GeckoDriver)"""  
@@ -94,9 +101,9 @@ def main():
     driver = chrome_setup(url)    
     keyword = "solo_search"
     simulate_search(driver, keyword)     
-    verify_results(driver, keyword)     
+    msg_url, msg_elem = verify_results(driver, keyword)     
     tear_down(driver)
-    send_results()
+    send_results(msg_url, msg_elem)
      
 
 if __name__=="__main__":
