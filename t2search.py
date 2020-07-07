08cb0cc42057413e9ac1f3041d080ce60c3351c2
Linda <stayathomegeek@gmail.com>
2020-07-07 10:51:36 -0400

    Completed code to find search dropdown; this version still runs only with Chrome

--- t2search.py
+++ t2search.py
@@ -6,8 +6,6 @@ NOTES:
 2. From Stackoverflow: HTMLTestRunner module combined with unittest provides basic but robust HTML reports.
 3. See https://stackoverflow.com/questions/34562061/webdriver-click-vs-javascript-click
  
-
- 
 ## may or may not need these ### 
 from   selenium.webdriver.support.ui import Select
 from   selenium.common.exceptions import NoAlertPresentException 
@@ -37,6 +35,10 @@ import logging
 import sys
 from datetime import datetime
 import time
+import requests
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By  
 
 class Search():
 
@@ -47,42 +49,91 @@ class Search():
         """This returns better information for the developer when he tries printing the instance"""
         return self.url 
 
-    def setUp(self):
-        driver = webdriver.Chrome("c:\\data\\chromedriver\\chromedriver.exe")
-        print("In setup", driver)
-        driver.get(self.url)
-        print("In setup", driver)
-        return driver
-         
+    def setUp(self, url):
+        try:
+            # Do  I need a message here because the driver IS found?
+            self.driver = webdriver.Chrome("c:\\data\\chromedriver\\chromedriver.exe")   
+            logging.info(f"{datetime.now(tz=None)} Search Chrome Info Chromedriver found")  
+        except (WebDriverException):
+            logging.info(f"{datetime.now(tz=None)} Search Chrome Fail Chromedriver not found. Process terminated")    
+            sys.exit(1)   
+        try:   
+            resp = requests.get(url)  
+            if resp.status_code == 200:                
+                self.driver.get(self.url)
+            logging.info(f"{datetime.now(tz=None)} Search Chrome Info {url} found")      
+        except:
+             
+            logging.info(f"{datetime.now(tz=None)} Search Chrome Fail URL {url} not found. Process terminated")    
+            self.driver.quit()
+            sys.exit(1)                 
+        return self.driver 
 
-    def verify_url(self, driver):       
+    def simulate_single_letter_search(self, driver, letter):
+        """Find the search box and type in a single letter which will force a dropdown"""
+        logging.info(f"{datetime.now(tz=None)} Search Chrome Info Looking for search box")
+        try:               
+            elem=driver.find_element(By.XPATH, '//*[@id="search-6"]/form/label/input') # We are looking inside the home page
+            logging.info(f"{datetime.now(tz=None)} Search Chrome Info Search box found")
+        except (NoSuchElementException):
+            logging.info(f"{datetime.now(tz=None)} Search Chrome Fail Search box not found")    
+            driver.quit()
+            sys.exit(1)
+        elem.send_keys(letter)  
+        elem.send_keys(Keys.ENTER)  
+        return
+ 
+    def find_dropdown(self,driver):
+        """See that we have a search suggestion dropdown"""
+        logging.info(f"{datetime.now(tz=None)} Search Chrome Info Looking for search suggestion dropdown")  
+        try:         
+            time.sleep(3)   
+            elem=driver.find_element(By.XPATH, '//*[@id="search-6"]/form/div')
+            logging.info(f"{datetime.now(tz=None)} Search Chrome Info Found search suggestion dropdown")   
+        except (NoSuchElementException):  
+            logging.info(f"{datetime.now(tz=None)} Search Chrome Fail Search suggestion dropdown not found")    
+            driver.quit()
+            sys.exit(1)     
+
+    def find_a_suggestion(self,driver):
+        """ Now that we've found the dropdown, it seems reasonable there should be at least one suggestion in the list"""    
+        logging.info(f"{datetime.now(tz=None)} Search Chrome Info Looking for one search suggestion") 
+        try:            
+            elem=driver.find_element(By.XPATH, '//*[@id="search-6"]/form/div/ul')
+            logging.info(f"{datetime.now(tz=None)} Search Chrome Info Found one search suggestion")   
+             
+        except (NoSuchElementException):             
+            logging.info(f"{datetime.now(tz=None)} Search Chrome Fail Search suggestion not found")    
+            driver.quit()
+            sys.exit(1)  
+
+
+    def verify_new_url(self, driver, new_url):       
         """Check on correct url which is https://solosegment.com/?s=solosegment_monitoring_test"""  
         time.sleep(3) # Needed this for firefox; otherwise it looks for: https://solosegment.com/#    
-        print(driver)
-        if driver.current_url == "https://www.python.org":           
-            logging.info(f"{datetime.now(tz=None)}  Pass")  
+                   
+        if driver.current_url == new_url:              
+            logging.info(f"{datetime.now(tz=None)} Pass")  
         else: 
-            logging.info(f"{datetime.now(tz=None)}  Fail")           
+            logging.info(f"{datetime.now(tz=None)} Fail with {driver.current_url}")           
         return driver  
 
-    def tearDown(self, driver):
-        print("in teardown", driver)
+    def tearDown(self, driver):         
         driver.close() 
         return  
 
-
 def main():
-    logging.basicConfig(filename='t12earch.log', level=logging.INFO)   
-    url = "https://www.python.org"  
-    mysearch = Search(url)  
-     
-    driver = mysearch.setUp()
-    print("In main", driver)
-    driver = mysearch.verify_url(driver)
-    print ("In main", driver)
-    mysearch.tearDown(driver)
-    print("done")
-    
+    logging.basicConfig(filename='t2search.log', level=logging.INFO)   
+    logging.warning("\n")
+    url = "https://solosegment.com/" 
+    mysearch = Search(url)       
+    driver = mysearch.setUp(url)    
+    mysearch.simulate_single_letter_search(driver, 's')    
+    mysearch.find_dropdown(driver)
+    mysearch.find_a_suggestion(driver) 
+    new_url = f"{url}?s=s"
+    driver = mysearch.verify_new_url(driver, new_url)     
+    mysearch.tearDown(driver)   
 
 
 if __name__ == "__main__":
