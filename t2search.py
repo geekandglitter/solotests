262057f187cd6bbf84af66abd47155fa6410caf1
Linda <stayathomegeek@gmail.com>
2020-07-19 12:37:29 -0400

    Removed Shamoy's code for now

--- t2search.py
+++ t2search.py
@@ -1,4 +1,3 @@
-
 # System and Module Imports
 import logging 
 import sys
@@ -22,33 +21,34 @@ from selenium.webdriver.edge.service import Service # couldn't get Service to wo
 from selenium.webdriver.safari.service import Service # untested of course
 from selenium.webdriver.ie.service import Service
 
+ 
 
-class MainInterfacer(): # <! --- HERE
-    def __init__(self, config):
-        self.driverSelect = {
-            "Firefox": self.setUpfirefox,
-            "Chrome": self.setUpchrome,
-            "Edge": self.setUpedge,
-            "IE": self.setUpIE,
-            "Safari": self.setUpsafari
-        }
-        self.driver = self.driverSelect[config["browser"]]() # Get Our Handler
-    
-    def setUpfirefox(self):
-        """Product name: Firefox Nightly Product version: 71.0a1 
-        Firefox can run headless with sendkeys. (Firefox handler is called GeckoDriver)"""  
-        options = Options()
-        options.headless = True
-        logging.info(f"{datetime.now(tz=None)} Info Looking for Firefox browser handler") 
-        
-        try:
-            service = Service('selenium_deps\\drivers\\geckodriver.exe') # Specify the custom path (new for Selenium 4)
-            handler = webdriver.Firefox(options=options, service=service)
-            logging.info(f"{datetime.now(tz=None)} Info Firefox browser handler found")             
-        except (WebDriverException):
-            logging.info(f"{datetime.now(tz=None)} Fail Firefox browser handler not found or failed to launch.")    
-            handler = None    
-        return handler   
+class Search():    
+    test_name = "Search 3" #class attribute       
+
+    def __init__(self, initial_url, results_url, browse, keyword):
+        self.keyword = keyword
+        self.initial_url = initial_url
+        self.results_url = results_url
+        self.browse=browse
+        if browse == "Chrome":  
+            handler = self.setUpchrome()   # get the handler
+        elif browse == "Firefox":
+            handler = self.setUpfirefox () # get the handler 
+        elif browse == "Edge":
+            handler = self.setUpedge()     # get the handler
+        elif browse == "IE":
+            handler = self.setUpIE()       # get the handler
+        elif browse == "Safari":
+            handler = self.setUpsafari()   # get the handler
+        self.handler=handler
+      
+
+    def __repr__(self):
+        """This returns better information for the developer when he tries printing the instance"""
+        return f"Initial URL is {self.initial_url}\nLanding URL is {self.results_url}"
+ 
+     
 
     def setUpchrome(self):
         """Product name: unavailable Product version: unavailable 
@@ -65,7 +65,23 @@ class MainInterfacer(): # <! --- HERE
         except (WebDriverException):
             logging.info(f"{datetime.now(tz=None)} Fail Chrome browser handler not found or failed to launch.")    
             handler = None  
-        return handler  
+        return handler         
+
+    def setUpfirefox(self):
+        """Product name: Firefox Nightly Product version: 71.0a1 
+        Firefox can run headless with sendkeys. (Firefox handler is called GeckoDriver)"""  
+        options = Options()
+        options.headless = True
+        logging.info(f"{datetime.now(tz=None)} Info Looking for Firefox browser handler") 
+        
+        try:
+            service = Service('selenium_deps\\drivers\\geckodriver.exe') # Specify the custom path (new for Selenium 4)
+            handler = webdriver.Firefox(options=options, service=service)
+            logging.info(f"{datetime.now(tz=None)} Info Firefox browser handler found")             
+        except (WebDriverException):
+            logging.info(f"{datetime.now(tz=None)} Fail Firefox browser handler not found or failed to launch.")    
+            handler = None    
+        return handler      
 
     def setUpedge(self):
         """Product name: Microsoft WebDriver Product version 83.0.478.58 
@@ -83,15 +99,32 @@ class MainInterfacer(): # <! --- HERE
         except (WebDriverException):
             logging.info(f"{datetime.now(tz=None)} Fail Edge browser handler not found or failed to launch.")    
             handler = None                       
-        return handler # ignore the handshake errors
-        
+        return handler # ignore the handshake errors 
+
+
+    def setUpsafari(self):
+        """I cannot currently test this safari code beause my only ios is on an old ipad Safari 12.4.7.  
+        I posted a writeup on implementing Safari that might be of some help. 
+        See https://speakingpython.blogspot.com/2020/07/working-with-selenium-webdriver-in.html
+        """ 
+        return
+        logging.info(f"{datetime.now(tz=None)} Info Looking for Safari browser handler")  
+        try:       
+            service = Service('selenium_deps\\drivers\\safaridriver.exe') # Specify the custom path (new for Selenium 4)       
+            handler = webdriver.Safari(options=options, service=service)    
+            logging.info(f"{datetime.now(tz=None)} Info Safari browser handler found") 
+        except (WebDriverException):
+            logging.info(f"{datetime.now(tz=None)} Fail Safari browser handler not found or failed to launch.")    
+            handler = None       
+        return handler 
+
     def setUpIE(self):
         """Product name: Selenium WebDriver Product version: 2.42.0.0
         IE does not have support for a headless mode
         IE has some other gotchas, too, which I posted in my blog. 
         See https://speakingpython.blogspot.com/2020/07/working-with-selenium-webdriver-in.html
         """    
-        logging.info(f"{datetime.now(tz=None)} Info looking for IE browser handler")   
+        logging.info(f"{datetime.now(tz=None)} Info Looking for IE browser handler")   
         try:   
             service = Service('selenium_deps\\drivers\\IEDriverServer.exe') # Specify the custom path (new for Selenium 4)  
             handler = webdriver.Ie(service=service)                     
@@ -101,42 +134,8 @@ class MainInterfacer(): # <! --- HERE
         except (WebDriverException):
             logging.info(f"{datetime.now(tz=None)} Fail IE browser handler not found or failed to launch.")    
             handler = None      
-        return handler        
+        return handler    
 
-    def setUpsafari(self):
-        """I cannot currently test this safari code beause my only ios is on an old ipad Safari 12.4.7.  
-        I posted a writeup on implementing Safari that might be of some help. 
-        See https://speakingpython.blogspot.com/2020/07/working-with-selenium-webdriver-in.html
-        """ 
-        logging.info(f"{datetime.now(tz=None)} Info Looking for Safari handler")  
-        try:       
-            service = Service('selenium_deps\\drivers\\safaridriver.exe') # Specify the custom path (new for Selenium 4)       
-            handler = webdriver.Safari(options=options, service=service)    
-            logging.info(f"{datetime.now(tz=None)} Info Safari browser handler found") 
-        except:
-            logging.info(f"{datetime.now(tz=None)} Fail Safari browser handler not found or failed to launch.")    
-            handler = None       
-        return handler 
-
-
-
-class Search(MainInterfacer):   # <! --- HERE
-    test_name = "Search 3" #class attribute       
-
-    def __init__(self, config):
-        super().__init__(config)
-        self.keyword = config['keyword']
-        self.initial_url = config['initial_url']
-        self.results_url = config['results_url']
-        self.browse = config['browser']
-
-        self.handler = self.driver
-        """From Bob: I personally would like to extend the init function more to let you pass in what driver you want to use as well, 
-        That way you can kind of pre-call all of the setup commands as well w/o having to know too deeply the inner workings of that. """
-
-    def __repr__(self):
-        """This returns better information for the developer when he tries printing the instance"""
-        return f"Initial URL is {self.initial_url}\nLanding URL is {self.results_url}"  
    
     def start_the_session(self):
         """See if the session is up and then get the session"""
@@ -147,7 +146,7 @@ class Search(MainInterfacer):   # <! --- HERE
                 self.handler.get(self.initial_url)
                 time.sleep(10)   
                 logging.info(f"{datetime.now(tz=None)} Info URL {self.initial_url} found") 
-                  
+                logging.info(f"{datetime.now(tz=None)} Info Session Initialized") 
         except:             
             logging.info(f"{datetime.now(tz=None)} Fail URL {self.initial_url} not found. Process terminated")    
             self.handler.quit()
@@ -181,14 +180,14 @@ class Search(MainInterfacer):   # <! --- HERE
             sys.exit(1)     
         return    
 
-    def find_a_suggestion(self):
-        """ Now that we've found the dropdown, it seems reasonable there should be at least one suggestion in the list"""    
-        logging.info(f"{datetime.now(tz=None)} Info Looking for one search suggestion") 
+    def find_search_suggestions(self):
+        """Finding one search suggestion is enough evidence that the dropdown is correct"""    
+        logging.info(f"{datetime.now(tz=None)} Info Looking for search suggestions") 
         try:            
             elem=self.handler.find_element(By.XPATH, '//*[@id="search-6"]/form/div/ul')
-            logging.info(f"{datetime.now(tz=None)} Info Found one search suggestion")                
+            logging.info(f"{datetime.now(tz=None)} Info Found search suggestions")                
         except (NoSuchElementException):             
-            logging.info(f"{datetime.now(tz=None)} Fail Search suggestion not found")    
+            logging.info(f"{datetime.now(tz=None)} Fail Search suggestions not found")    
             self.handler.quit()
             sys.exit(1)  
         return    
@@ -199,7 +198,7 @@ class Search(MainInterfacer):   # <! --- HERE
         if self.handler.current_url == self.results_url:              
             logging.info(f"{datetime.now(tz=None)} Pass")  
         else: 
-            logging.info(f"{datetime.now(tz=None)} Fail with {self.handler.current_url} not equal to {self.results_url}")           
+            logging.info(f"{datetime.now(tz=None)} Fail with {self.handler.current_url} not equal to the expected ur: {self.results_url}")           
         return    
 
     def tearDown(self):    
@@ -214,54 +213,21 @@ def main():
     About this script: https://speakingpython.blogspot.com/2020/07/working-with-selenium-webdriver-in.html
     """
 
-    logging.basicConfig(filename='t2search.log', level=logging.INFO)     
-    
-    config = {
-        "Firefox": {
-            "initial_url": "https://solosegment.com/",
-            "keyword": "testing",
-            "results_url": "https://solosegment.com/?s=testing",
-            "browser": "Firefox"
-        },
-        "Chrome": {
-            "initial_url": "https://solosegment.com/",
-            "keyword": "testing",
-            "results_url": "https://solosegment.com/?s=testing",
-            "browser": "Chrome"
-        },
-        "Edge": {
-            "initial_url": "https://solosegment.com/",
-            "keyword": "testing",
-            "results_url": "https://solosegment.com/?s=testing",
-            "browser": "Edge"
-        },
-        "IE": {
-            "initial_url": "https://solosegment.com/",
-            "keyword": "testing",
-            "results_url": "https://solosegment.com/?s=testing",
-            "browser": "IE"
-        },
-        "Safari": {
-            "initial_url": "https://solosegment.com/",
-            "keyword": "testing",
-            "results_url": "https://solosegment.com/?s=testing",
-            "browser": "Safari"
-        }
+    logging.basicConfig(filename='t2search.log', level=logging.INFO)      
+    initial_url = "https://solosegment.com/" 
+    results_url = f"{initial_url}?s=s" #class attribute
+    keyword = "s"
 
-    }   
-
-
-    for b in config:         
-
-        mysearch = Search(config[b])
+    for browse in  [ "Chrome", "Firefox", "Edge","IE"]:      #for browse in  [ "Chrome", "Firefox", "Edge","IE", "Safari"]:           
+        mysearch = Search(initial_url, results_url, browse, keyword)     
+         
         if mysearch.handler == None: # In the event that the handler is not found or failed to launch,
             continue # go on to the next browser
         mysearch.start_the_session()          # start the session we want to test
         mysearch.simulate_keyword_entry()    
         mysearch.find_dropdown()
-        mysearch.find_a_suggestion()
+        mysearch.find_search_suggestions()
         mysearch.verify_results_url()     
-        mysearch.tearDown()
-
+        mysearch.tearDown()  
 if __name__ == "__main__":
     main()
